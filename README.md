b993ca9fbab13709b2c97af7b2eb4608a2eff70b
Appsuccessor_49 <vikas@appanalytics.in>
2022-09-20 23:11:05 +0530

    Updated requirements and others as Needed For Project

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Deep Classifier Project 
 DeepLearningCNN
 End to end project for CNN 
+
+
+## workflow
+
+1. Update config.yaml
+2. Update secrets.yaml [Optional]
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config.
+6. Update the components
+7. Update the pipeline
+8. Test run pipeline stage
+9. run tox for testing your package
+10. Update the dvc.yaml
+11. run "dvc repro" for running all the stages in pipeline
+
+![img](https://raw.githubusercontent.com/c17hawke/FSDS_NOV_deepCNNClassifier/main/docs/images/Data%20Ingestion%402x%20(1).png)
--- template.py
+++ template.py
@@ -1,53 +1,48 @@
 import os
 from pathlib import Path
-import logging 
+import logging
 
-
-# logging.basicConfig(level="INFO", format='[%(asctime)s] : %(message)s : ')
-logging.basicConfig(level=20, format='[%(asctime)s] : %(message)s : ')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+# logging.basicConfig(level=20, format='[%(asctime)s] : %(message)s : ')
 
 package_name = "deepClassifier"
 
-
 list_of_files = [
-    ".github/workflows/.gitkeep",
-    f"src/{package_name}/__init__.py",
-    f"src/{package_name}/components/__init__.py",
-    f"src/{package_name}/utils/__init__.py",
-    f"src/{package_name}/config/__init__.py",
-    f"src/{package_name}/pipeline/__init__.py",
-    f"src/{package_name}/entity/__init__.py",
-    f"src/{package_name}/constants/__init__.py",
-    "tests/__init__.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "configs/config.yaml",
-    "dvc.yaml",
-    "params.yaml",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
-    "research/trials.ipynb",
+   ".github/workflows/.gitkeep",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/components/__init__.py", 
+   f"src/{package_name}/utils/__init__.py", 
+   f"src/{package_name}/config/__init__.py", 
+   f"src/{package_name}/pipeline/__init__.py", 
+   f"src/{package_name}/entity/__init__.py", 
+   f"src/{package_name}/constants/__init__.py",
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "configs/config.yaml",
+   "dvc.yaml",
+   "params.yaml",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "research/trials.ipynb", 
 ]
-#Data Version Control dvc
-#tox for testing your projects locally 
 
-
-for filpath in list_of_files:
-    filepath = Path(filpath)
+for filepath in list_of_files:
+    filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    if filedir !="":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating Directory : {filename} for file : {filepath}")
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass # Create an Empty File
-            logging.info(f"Creating empty file : {filename}")
-    else:
-        logging.info(f"File {filename} already Exist ")
+            pass # create an empty file
+            logging.info(f"Creating empty file: {filepath}")
 
+    else:
+        logging.info(f"{filename} already exists")
